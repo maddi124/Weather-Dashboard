397efa006615271e62315e4dd23e34d4600d3a45
Madeline Garcia-Cabrera <vmgarcia.cabrera21@gmail.com>
2021-12-08 18:59:59 -0500

    fixed localstorage value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,39 @@
-
-function weathersearch(){
+var historylist=document.querySelector('#history');
+var divEl = document.createElement("div");
+// var APIkey ='4e460ef796224efaf5dab593e57a5787';
+// var endpoint ='api.openweathermap.org';
+$(document).ready(function(){
 
 $('.btn').on('click',function(){
-    console.log('this was click');
+    console.log('this is search button');
+    // var cityinput =$('#city-input').val();
+    //     console.log (cityinput);
+
     var text= $(this).siblings('.city-input').val();
+        console.log(text);
     var card= $(this).parent().attr('id')
+        console.log(card);
+    localStorage.setItem('card',JSON.stringify(text));
+    // weather(cityinput);
 
-    localStorage.setItem(text,card)
+    var list =JSON.parse(localStorage.getItem('card'));
+        console.log(list);
 
+    // var searchDiv = $("<button class='list-group-item m-2 col-12'>").text(list);
+    // var psearch= $('<div>');
+    // psearch.append(searchDiv)
+    // $('#history').prepend(psearch);        
 });
 
+//function weathersearch(){}
+
+function deletelist(){
+$('.delete').on('click',function(){
+    console.log('this is the clear button');
+});
 }
-weathersearch();
+//weathersearch();
+});
+
+
+// localStorage.clear();
